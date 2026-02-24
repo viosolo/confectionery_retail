f0607549270688dd5eb8279791c136663d81fa4f
viosolo <solonovich.violetta@mail.ru>
2026-02-25 00:37:07 +0300

    First lab that passed all checkstyle

--- backend/src/main/java/com/example/confectionery/service/ProductService.java
+++ backend/src/main/java/com/example/confectionery/service/ProductService.java
@@ -4,47 +4,49 @@ import com.example.confectionery.dto.ProductDto;
 import com.example.confectionery.entity.ProductType;
 import com.example.confectionery.mapper.ProductDtoMapper;
 import com.example.confectionery.repository.ProductRepository;
+
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
-  private final ProductRepository productRepository;
-  private final ProductDtoMapper productDtoMapper;
+    private final ProductRepository productRepository;
+    private final ProductDtoMapper productDtoMapper;
 
-  public ProductService(ProductRepository productRepository,
-                        ProductDtoMapper productDtoMapper) {
-    this.productRepository = productRepository;
-    this.productDtoMapper = productDtoMapper;
-  }
+    public ProductService(ProductRepository productRepository,
+                          ProductDtoMapper productDtoMapper) {
+        this.productRepository = productRepository;
+        this.productDtoMapper = productDtoMapper;
+    }
 
-  public List<ProductDto> getAllProducts() {
-    return productRepository.findAllProducts()
-        .stream()
-        .map(productDtoMapper)
-        .toList();
-  }
+    public List<ProductDto> getAllProducts() {
+        return productRepository.findAllProducts()
+                .stream()
+                .map(productDtoMapper)
+                .toList();
+    }
 
-  public ProductDto getProductById(Long id) {
-    return productRepository.findProductById(id)
-        .map(productDtoMapper)
-        .orElseThrow(() -> new RuntimeException(
-            "product with id [%s] not found".formatted(id)
-        ));
-  }
+    public ProductDto getProductById(Long id) {
+        return productRepository.findProductById(id)
+                .map(productDtoMapper)
+                .orElseThrow(() -> new RuntimeException(
+                        "product with id [%s] not found".formatted(id)
+                ));
+    }
 
-  public List<ProductDto> getProductsByType(ProductType type) {
-    return productRepository.findAllProducts()
-        .stream()
-        .filter(product -> product.getType() == type)
-        .map(productDtoMapper)
-        .toList();
-  }
+    public List<ProductDto> getProductsByType(ProductType type) {
+        return productRepository.findAllProducts()
+                .stream()
+                .filter(product -> product.getType() == type)
+                .map(productDtoMapper)
+                .toList();
+    }
 
-  public ProductDto getProductByName(String name) {
-    return productRepository.findByName(name)
-        .map(productDtoMapper)
-        .orElseThrow(() -> new RuntimeException("Product not found"));
-  }
+    public ProductDto getProductByName(String name) {
+        return productRepository.findByName(name)
+                .map(productDtoMapper)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
 }
