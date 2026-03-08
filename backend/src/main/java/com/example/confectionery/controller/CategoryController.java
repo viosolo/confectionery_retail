206a7becc4d26869a6c89a28f6263df037ba2602
viosolo <solonovich.violetta@mail.ru>
2026-03-08 18:26:51 +0300

    Lab 3 with category

--- backend/src/main/java/com/example/confectionery/controller/CategoryController.java
+++ backend/src/main/java/com/example/confectionery/controller/CategoryController.java
@@ -2,17 +2,22 @@ package com.example.confectionery.controller;
 
 import com.example.confectionery.dto.CategoryRequest;
 import com.example.confectionery.dto.CategoryResponse;
+import com.example.confectionery.dto.ProductResponse;
 import com.example.confectionery.service.CategoryService;
+import com.example.confectionery.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,6 +27,7 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final ProductService productService;
 
     @GetMapping
     public List<CategoryResponse> getAll() {
@@ -32,6 +38,16 @@ public class CategoryController {
     public ResponseEntity<CategoryResponse> create(@RequestBody CategoryRequest request) {
         CategoryResponse response = categoryService.saveCategory(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<Page<ProductResponse>> getProductsByFilter(
+            @RequestParam(name = "slug") String slug,
+            @RequestParam(required = false) List<String> flavors,
+            @RequestParam(required = false) Double maxPrice,
+            Pageable pageable) {
+
+        return ResponseEntity.ok(productService.searchWithCache(slug, flavors, maxPrice, pageable));
     }
 
     @GetMapping("/{id}")
