771ed46f47b321ea8ad251e869fa70cf10024931
viosolo <solonovich.violetta@mail.ru>
2026-03-31 10:38:54 +0300

    Lab 6 with all fixed checkstyle

--- backend/src/test/java/com/example/confectionery/service/AsyncOrderServiceTest.java
+++ backend/src/test/java/com/example/confectionery/service/AsyncOrderServiceTest.java
@@ -7,7 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.example.confectionery.dto.OrderRequestDto;
 import com.example.confectionery.dto.OrderResponseDto;
