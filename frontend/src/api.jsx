f572509aa2bdd9241b031daecfda6891a2ba3d36
viosolo <solonovich.violetta@mail.ru>
2026-04-19 14:07:23 +0300

    For railway

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: window.location.hostname === 'localhost'
+        ? 'http://localhost:8080/api'
+        : 'https://confectioneryretail-production.up.railway.app/api'
 });
 
 export default api;
